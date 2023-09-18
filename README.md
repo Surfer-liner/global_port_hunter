94c542d858e88e46e5fbb111c5444f648e96b8d4
surfer_liner <example@example.com>
2023-09-18 08:07:46 +0600

    started the project

---
+++ README.md
@@ -0,0 +1,53 @@
+# Multiprocessing Port Scanner
+
+This Python script scans multiple IP addresses and ports using multiprocessing, making it useful for checking the accessibility of various ports on a range of IP addresses simultaneously.
+
+## Features
+
+- Scans a wide range of IP addresses and ports concurrently.
+- Logs scan results, including open ports, to a specified log file.
+- Utilizes multiprocessing for improved scan speed.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- Python 3.x installed on your system.
+- Required Python modules: `datetime`, `socket`, `logging`, `multiprocessing`, `time`.
+
+## Usage
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/surfer_liner/global_port_hunter.git
+
+2. Modify the script settings (optional):
+
+You can customize the script by modifying the following settings in the script itself:
+
+- log_path: The path to the log file where scan results will be recorded.
+- open_sockets: The path to the file where open ports will be recorded.
+
+3. Run the script:
+
+   ```bash
+   python3 mp_requester.py
+
+4. Monitor the progress:
+The script will start scanning IP addresses and ports, displaying open ports in the terminal. The progress will also be logged in the specified log file.
+
+5. Check the results:
+After the scan is complete, you can review the log file to see the list of open ports on the scanned IP addresses.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or want to enhance the script, please create a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+## Acknowledgments
+
+Thanks to the Python community for creating and maintaining the necessary libraries.
---
+++ mp_requester.py
@@ -0,0 +1,139 @@
+import datetime
+import socket
+import logging
+import multiprocessing
+import time
+
+# FIXME ———————————————————————————————————————————————————————— SETTINGS ——————
+log_path = '/Users/surfer_liner/python_/projects/test/mp_requester.log'
+open_sockets = '/Users/surfer_liner/python_/projects/test/open_sockets.txt'
+# FIXME ————————————————————————————————————————————————————————————————————————
+
+
+# Installing the application output
+logging.basicConfig(filename=log_path, level=logging.INFO)
+
+
+def ip_generator(queue):
+    '''
+    Generates an ip address with a port
+
+    Args:
+        None
+    Return:
+        None
+    '''
+    try:
+        # Install the ports we want to check
+        ports_list = [22, 23, 25, 80, 137, 138, 139, 443, 445, 3306, 5432]
+
+        # Creating an IP address string
+        for el0 in range(1, 191):
+            for el1 in range(1, 256):
+                for el2 in range(1, 256):
+                    for el3 in range(1, 256):
+                        for port in ports_list:
+                            ip = f'{el0}.{el1}.{el2}.{el3}'
+                            queue.put((ip, port))
+
+    # Catching an error in the function
+    except Exception as e:
+        # Getting the current time
+        current_time = datetime.datetime.now()
+        # Format the time
+        formatted_time = current_time.strftime('%H:%M:%S')
+        # Fix error in the log
+        logging.error(f'{formatted_time} ERR ip_generator: {e}')
+
+
+def ip_checker(queue):
+    '''
+    Checks the port of the received address for openness
+    If the port is open, it fixes the address in open_sockets.txt
+
+    Args:
+        None
+    Returns:
+        None
+    '''
+    try:
+        # Creating a client socket
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Setting the waiting period for a response from the server
+        client_socket.settimeout(1)
+
+        # Extract IP and port
+        count = 0
+        while not queue.empty():
+            ip, port = queue.get()
+            count += 1
+
+            if count % 10_000_000 == 0:
+                # Forming the current time
+                current_time = datetime.datetime.now()
+                # Format the time
+                formatted_time = current_time.strftime('%H:%M:%S')
+                # Format count
+                formatted_count = '{:,}'.format(count)
+                # Fix it in the logs
+                logging.info(
+                    f"{formatted_time} {formatted_count} ip's was checked "
+                    f"by ip_checker, current IP = {ip}:{port}")
+
+            try:
+                # Establishing a connection to the server
+                server_answer = client_socket.connect_ex((ip, port))
+                # If there is an answer
+                if server_answer == 0:
+                    # Output the address
+                    print(f'{ip}:{port} is open')
+                    # Fixing the status of the port
+                    with open(open_sockets, 'a+') as f:
+                        f.write(f'{ip}:{port} is open\n')
+            except socket.timeout:
+                pass
+
+        # Closing the socket
+        client_socket.close()
+
+    # Catching an error in the function
+    except Exception as e:
+        # Extracting IP and PORT from the queue
+        ip, port = queue.get()
+        # Forming the current time
+        current_time = datetime.datetime.now()
+        # Format the time
+        formatted_time = current_time.strftime('%H:%M:%S')
+        # Fix it in the logs
+        logging.error(f'{formatted_time} ERR ip_checker: {e}, {ip}:{port}')
+
+
+if __name__ == '__main__':
+    # Determine the number of processes
+    num_processes = 10
+    processes = []
+
+    # Creating queue
+    queue = multiprocessing.Queue()
+
+    # Creating a process to fill the queue
+    generator_process = multiprocessing.Process(target=ip_generator, args=(queue, ))
+    generator_process.start()
+
+    # Waiting for the queue to fill
+    time.sleep(1)
+
+    # Starting the list of processes
+    for _ in range(num_processes):
+        process = multiprocessing.Process(target=ip_checker, args=(queue, ))
+        process.start()
+        processes.append(process)
+
+    # Completing the generation process
+    generator_process.join()
+
+    # Completing the checking processes
+    for process in processes:
+        process.join()
+
+    print('all processes has been finished')
